36a104f84927777acfde6b1c8fef2d662f2b856f
Eduardo Mattos <edukmattos@gmail.com>
2021-04-19 15:44:28 -0300

    SingInPage - controllers

--- lib/app/modules/auth/sign_in/sign_in_page.dart
+++ lib/app/modules/auth/sign_in/sign_in_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
@@ -16,9 +17,6 @@ class SignInPageState extends ModularState<SignInPage, SignInStore> {
   final _scaffoldKey = GlobalKey<FormState>();
 
   Widget _buildEmailTF() {
-    //return Observer(
-    //  name: 'observerEmail',
-    //  builder: (_) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
@@ -33,104 +31,119 @@ class SignInPageState extends ModularState<SignInPage, SignInStore> {
           alignment: Alignment.centerLeft,
           decoration: kBoxDecorationStyle,
           height: 45,
-          child: Observer(
-            name: 'observerEmail',
-            builder: (_) {
-              return TextFormField(
-                autofocus: true,
-                onChanged: controller.changeEmail,
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  labelText: 'E-mail',
-                  labelStyle: kLabelStyle,
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.transparent,
-                    ),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                    ),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  contentPadding: EdgeInsets.only(
-                    top: 14,
-                  ),
-                  prefixIcon: Icon(
-                    Icons.email,
-                    color: Colors.white,
-                  ),
-                  //hintText: "Entre com seu e-mail",
-                  //hintStyle: kHintTextStyle,
+          child: TextFormField(
+            autofocus: true,
+            onChanged: controller.changeEmail,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              labelText: 'E-mail',
+              labelStyle: kLabelStyle,
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.transparent,
                 ),
-              );
-            },
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.white,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              contentPadding: EdgeInsets.only(
+                top: 14,
+              ),
+              prefixIcon: Icon(
+                Icons.email,
+                color: Colors.white,
+              ),
+              //hintText: "Entre com seu e-mail",
+              //hintStyle: kHintTextStyle,
+            ),
           ),
-        )
+        ),
       ],
     );
-    //},
-    //);
   }
 
   Widget _buildPasswordTF() {
-    return Observer(
-      name: 'observerPassword',
-      builder: (_) {
-        return Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            //Text(
-            //  "Password",
-            //  style: kLabelStyle,
-            //),
-            SizedBox(
-              height: 10,
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              decoration: kBoxDecorationStyle,
-              height: 45,
-              child: TextFormField(
-                autofocus: false,
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  labelStyle: kLabelStyle,
-                  contentPadding: EdgeInsets.only(
-                    top: kDefaultPadding * 0.0,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        //Text(
+        //  'E-mail',
+        //  style: kLabelStyle,
+        //),
+        //SizedBox(height: kDefaultPadding * 0.5),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: kBoxDecorationStyleLight,
+          height: kDefaultPadding * 2.5,
+          child: Padding(
+            padding: EdgeInsets.only(top: kDefaultPadding * 0.00),
+            child: TextFormField(
+              onChanged: controller.changePassword,
+              autofocus: false,
+              obscureText: true,
+              keyboardType: TextInputType.text,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.transparent,
                   ),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.transparent,
-                    ),
-                    borderRadius: BorderRadius.circular(10.0),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.white,
                   ),
-                  focusedBorder: OutlineInputBorder(
-                    borderSide: BorderSide(
-                      color: Colors.white,
-                    ),
-                    borderRadius: BorderRadius.circular(10.0),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                //border: InputBorder.none,
+                labelText: "Password",
+                labelStyle: kLabelStyle,
+                contentPadding: EdgeInsets.only(
+                  top: kDefaultPadding * 0.0,
+                ),
+
+                prefixIcon: Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: kDefaultPadding * 0.0,
+                    vertical: kDefaultPadding * 0.0,
                   ),
-                  //contentPadding: EdgeInsets.only(
-                  //  top: 14,
-                  //),
-                  prefixIcon: Icon(
+                  child: Icon(
                     Icons.lock,
                     color: Colors.white,
                   ),
-                  //hintText: "Entre com a sua senha  ",
-                  //hintStyle: kHintTextStyle,
                 ),
+                suffixIcon: Padding(
+                  padding: EdgeInsetsDirectional.only(
+                    top: kDefaultPadding * 1.00,
+                    end: kDefaultPadding * 0.25,
+                  ),
+                  //child: controller.validatePassword() == null
+                  //    ? null
+                  //    : Text(
+                  //        controller.validatePassword(),
+                  //        style: TextStyle(
+                  //            color: Colors.red,
+                  //            fontFamily: 'OpenSans',
+                  //            fontSize: 12),
+                  //      ),
+                ),
+                //hintText: 'E-mail',
+                //hintStyle: kHintTextStyle,
+
+                //errorText: controller.validateEmail(),
               ),
-            )
-          ],
-        );
-      },
+            ),
+          ),
+        ),
+      ],
     );
   }
 
@@ -141,61 +154,49 @@ class SignInPageState extends ModularState<SignInPage, SignInStore> {
       //  title: Text(widget.title),
       //),
       key: _scaffoldKey,
-      body: Stack(
-        children: <Widget>[
-          Container(
-            height: double.infinity,
-            width: double.infinity,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color(0xFF23AEF5),
-                  Color(0xFF23AEF5),
-                  Color(0xFF23AEF5),
-                  Color(0xFF23AEF5),
-                ],
-                stops: [0.1, 0.4, 0.7, 0.9],
-              ),
-            ),
-          ),
-          Container(
-            height: double.infinity,
-            child: SingleChildScrollView(
-              physics: AlwaysScrollableScrollPhysics(),
-              padding: EdgeInsets.symmetric(
-                horizontal: 40,
-                vertical: 120,
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    "Sign In",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontFamily: "OpenSans",
-                      fontSize: 30,
-                      fontWeight: FontWeight.bold,
+      body: Form(
+        child: AnnotatedRegion<SystemUiOverlayStyle>(
+          value: SystemUiOverlayStyle.light,
+          child: GestureDetector(
+            onTap: () => FocusScope.of(context).unfocus(),
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  decoration: kBoxDecorationStyleBackgroundLight,
+                ),
+                Container(
+                  height: double.infinity,
+                  child: SingleChildScrollView(
+                    physics: AlwaysScrollableScrollPhysics(),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: kDefaultPadding,
+                      vertical: kDefaultPadding,
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          "Sing In",
+                          style: kFontTextStyle30,
+                        ),
+                        SizedBox(
+                          height: kDefaultPadding * 0.5,
+                        ),
+                        _buildEmailTF(),
+                        SizedBox(
+                          height: kDefaultPadding * 1.0,
+                        ),
+                        _buildPasswordTF(),
+                      ],
                     ),
                   ),
-                  SizedBox(
-                    height: 30,
-                  ),
-                  _buildEmailTF(),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  //_buildPasswordTF(),
-                  SizedBox(
-                    height: 10,
-                  ),
-                ],
-              ),
+                )
+              ],
             ),
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
