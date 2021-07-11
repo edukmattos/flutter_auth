fff121406043103117e0924dafc345f6c1a7c9b2
Eduardo Mattos <edukmattos@gmail.com>
2021-07-11 17:37:27 -0300

    Settings Page

--- lib/app/modules/settings/settings_page.dart
+++ lib/app/modules/settings/settings_page.dart
@@ -39,8 +39,17 @@ class SettingsPageState extends ModularState<SettingsPage, SettingsStore> {
         SettingsSection(
           title: 'Account',
           tiles: [
-            SettingsTile(title: 'Phone number', leading: Icon(Icons.phone)),
-            SettingsTile(title: 'Email', leading: Icon(Icons.email)),
+            SettingsTile(
+              title: '51 99408-0770',
+              subtitle: "Celular",
+              trailing: Icon(Icons.edit_outlined),
+              leading: Icon(Icons.phone),
+              onPressed: (_) {
+                print("TESTE");
+              },
+            ),
+            SettingsTile(
+                title: 'edukmattos@gmail.com', leading: Icon(Icons.email)),
             SettingsTile(title: 'Sign out', leading: Icon(Icons.exit_to_app)),
           ],
         ),
@@ -80,13 +89,14 @@ class SettingsPageState extends ModularState<SettingsPage, SettingsStore> {
           ],
         ),
         SettingsSection(
-          title: 'Misc',
+          title: 'Recursos',
           tiles: [
             SettingsTile(
-                title: 'Terms of Service', leading: Icon(Icons.description)),
+                title: 'Jurandir, Velson, Janaína',
+                leading: Icon(Icons.people)),
             SettingsTile(
-                title: 'Open source licenses',
-                leading: Icon(Icons.collections_bookmark)),
+                title: 'IHG78E4 - 21/04/2021: 12.345Km',
+                leading: Icon(Icons.car_rental)),
           ],
         ),
         CustomSection(
@@ -95,7 +105,7 @@ class SettingsPageState extends ModularState<SettingsPage, SettingsStore> {
               Padding(
                 padding: const EdgeInsets.only(top: 22, bottom: 8),
                 child: Image.asset(
-                  'assets/settings.png',
+                  'assets/images/app/settings.png',
                   height: 50,
                   width: 50,
                   color: Color(0xFF777777),
@@ -148,110 +158,7 @@ class SettingsPageState extends ModularState<SettingsPage, SettingsStore> {
       appBar: appBar,
       //backgroundColor: Color(0xFF61A8EB),
       body: SafeArea(
-        child: SettingsList(
-          sections: [
-            SettingsSection(
-              title: 'Common',
-              tiles: [
-                SettingsTile(
-                  title: 'Language',
-                  subtitle: 'English',
-                  leading: Icon(Icons.language),
-                  onPressed: (context) {},
-                ),
-                SettingsTile(
-                  title: 'Environment',
-                  subtitle: 'Production',
-                  leading: Icon(Icons.cloud_queue),
-                ),
-              ],
-            ),
-            SettingsSection(
-              title: 'Account',
-              tiles: [
-                SettingsTile(
-                  title: '51 99408-0770',
-                  subtitle: "Celular",
-                  trailing: Icon(Icons.edit_outlined),
-                  leading: Icon(Icons.phone),
-                  onPressed: (_) {
-                    print("TESTE");
-                  },
-                ),
-                SettingsTile(
-                    title: 'edukmattos@gmail.com', leading: Icon(Icons.email)),
-                SettingsTile(
-                    title: 'Sign out', leading: Icon(Icons.exit_to_app)),
-              ],
-            ),
-            SettingsSection(
-              title: 'Security',
-              tiles: [
-                SettingsTile.switchTile(
-                  title: 'Lock app in background',
-                  leading: Icon(Icons.phonelink_lock),
-                  switchValue: lockInBackground,
-                  onToggle: (bool value) {
-                    setState(() {
-                      lockInBackground = value;
-                      notificationsEnabled = value;
-                    });
-                  },
-                ),
-                SettingsTile.switchTile(
-                    title: 'Use fingerprint',
-                    subtitle:
-                        'Allow application to access stored fingerprint IDs.',
-                    leading: Icon(Icons.fingerprint),
-                    onToggle: (bool value) {},
-                    switchValue: false),
-                SettingsTile.switchTile(
-                  title: 'Change password',
-                  leading: Icon(Icons.lock),
-                  switchValue: true,
-                  onToggle: (bool value) {},
-                ),
-                SettingsTile.switchTile(
-                  title: 'Enable Notifications',
-                  enabled: notificationsEnabled,
-                  leading: Icon(Icons.notifications_active),
-                  switchValue: true,
-                  onToggle: (value) {},
-                ),
-              ],
-            ),
-            SettingsSection(
-              title: 'Recursos',
-              tiles: [
-                SettingsTile(
-                    title: 'Jurandir, Velson, Janaína',
-                    leading: Icon(Icons.people)),
-                SettingsTile(
-                    title: 'IHG78E4 - 21/04/2021: 12.345Km',
-                    leading: Icon(Icons.car_rental)),
-              ],
-            ),
-            CustomSection(
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(top: 22, bottom: 8),
-                    child: Image.asset(
-                      'assets/images/app/settings.png',
-                      height: 50,
-                      width: 50,
-                      color: Color(0xFF777777),
-                    ),
-                  ),
-                  Text(
-                    'Version: 2.4.0 (287)',
-                    style: TextStyle(color: Color(0xFF777777)),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+        child: _buildSettingsList(),
       ),
       drawer: AppDrawerWidget(),
     );
