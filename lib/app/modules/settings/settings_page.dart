4ed30d6b3cf9c20d6f31f2cc74d2a0ec44586b00
Eduardo Mattos <edukmattos@gmail.com>
2021-07-11 17:32:27 -0300

    Settings Page

--- lib/app/modules/settings/settings_page.dart
+++ lib/app/modules/settings/settings_page.dart
@@ -2,6 +2,7 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 import '../../shared/widgets/drawer/app_drawer_widget.dart';
 import 'settings_store.dart';
@@ -15,6 +16,101 @@ class SettingsPage extends StatefulWidget {
 class SettingsPageState extends ModularState<SettingsPage, SettingsStore> {
   bool lockInBackground = true;
   bool notificationsEnabled = true;
+
+  Widget _buildSettingsList() {
+    return SettingsList(
+      sections: [
+        SettingsSection(
+          title: 'Common',
+          tiles: [
+            SettingsTile(
+              title: 'Language',
+              subtitle: 'English',
+              leading: Icon(Icons.language),
+              onPressed: (context) {},
+            ),
+            SettingsTile(
+              title: 'Environment',
+              subtitle: 'Production',
+              leading: Icon(Icons.cloud_queue),
+            ),
+          ],
+        ),
+        SettingsSection(
+          title: 'Account',
+          tiles: [
+            SettingsTile(title: 'Phone number', leading: Icon(Icons.phone)),
+            SettingsTile(title: 'Email', leading: Icon(Icons.email)),
+            SettingsTile(title: 'Sign out', leading: Icon(Icons.exit_to_app)),
+          ],
+        ),
+        SettingsSection(
+          title: 'Security',
+          tiles: [
+            SettingsTile.switchTile(
+              title: 'Lock app in background',
+              leading: Icon(Icons.phonelink_lock),
+              switchValue: lockInBackground,
+              onToggle: (bool value) {
+                setState(() {
+                  lockInBackground = value;
+                  notificationsEnabled = value;
+                });
+              },
+            ),
+            SettingsTile.switchTile(
+                title: 'Use fingerprint',
+                subtitle: 'Allow application to access stored fingerprint IDs.',
+                leading: Icon(Icons.fingerprint),
+                onToggle: (bool value) {},
+                switchValue: false),
+            SettingsTile.switchTile(
+              title: 'Change password',
+              leading: Icon(Icons.lock),
+              switchValue: true,
+              onToggle: (bool value) {},
+            ),
+            SettingsTile.switchTile(
+              title: 'Enable Notifications',
+              enabled: notificationsEnabled,
+              leading: Icon(Icons.notifications_active),
+              switchValue: true,
+              onToggle: (value) {},
+            ),
+          ],
+        ),
+        SettingsSection(
+          title: 'Misc',
+          tiles: [
+            SettingsTile(
+                title: 'Terms of Service', leading: Icon(Icons.description)),
+            SettingsTile(
+                title: 'Open source licenses',
+                leading: Icon(Icons.collections_bookmark)),
+          ],
+        ),
+        CustomSection(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(top: 22, bottom: 8),
+                child: Image.asset(
+                  'assets/settings.png',
+                  height: 50,
+                  width: 50,
+                  color: Color(0xFF777777),
+                ),
+              ),
+              Text(
+                'Version: 2.4.0 (287)',
+                style: TextStyle(color: Color(0xFF777777)),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -52,11 +148,109 @@ class SettingsPageState extends ModularState<SettingsPage, SettingsStore> {
       appBar: appBar,
       //backgroundColor: Color(0xFF61A8EB),
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[],
-          ),
+        child: SettingsList(
+          sections: [
+            SettingsSection(
+              title: 'Common',
+              tiles: [
+                SettingsTile(
+                  title: 'Language',
+                  subtitle: 'English',
+                  leading: Icon(Icons.language),
+                  onPressed: (context) {},
+                ),
+                SettingsTile(
+                  title: 'Environment',
+                  subtitle: 'Production',
+                  leading: Icon(Icons.cloud_queue),
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: 'Account',
+              tiles: [
+                SettingsTile(
+                  title: '51 99408-0770',
+                  subtitle: "Celular",
+                  trailing: Icon(Icons.edit_outlined),
+                  leading: Icon(Icons.phone),
+                  onPressed: (_) {
+                    print("TESTE");
+                  },
+                ),
+                SettingsTile(
+                    title: 'edukmattos@gmail.com', leading: Icon(Icons.email)),
+                SettingsTile(
+                    title: 'Sign out', leading: Icon(Icons.exit_to_app)),
+              ],
+            ),
+            SettingsSection(
+              title: 'Security',
+              tiles: [
+                SettingsTile.switchTile(
+                  title: 'Lock app in background',
+                  leading: Icon(Icons.phonelink_lock),
+                  switchValue: lockInBackground,
+                  onToggle: (bool value) {
+                    setState(() {
+                      lockInBackground = value;
+                      notificationsEnabled = value;
+                    });
+                  },
+                ),
+                SettingsTile.switchTile(
+                    title: 'Use fingerprint',
+                    subtitle:
+                        'Allow application to access stored fingerprint IDs.',
+                    leading: Icon(Icons.fingerprint),
+                    onToggle: (bool value) {},
+                    switchValue: false),
+                SettingsTile.switchTile(
+                  title: 'Change password',
+                  leading: Icon(Icons.lock),
+                  switchValue: true,
+                  onToggle: (bool value) {},
+                ),
+                SettingsTile.switchTile(
+                  title: 'Enable Notifications',
+                  enabled: notificationsEnabled,
+                  leading: Icon(Icons.notifications_active),
+                  switchValue: true,
+                  onToggle: (value) {},
+                ),
+              ],
+            ),
+            SettingsSection(
+              title: 'Recursos',
+              tiles: [
+                SettingsTile(
+                    title: 'Jurandir, Velson, Janaína',
+                    leading: Icon(Icons.people)),
+                SettingsTile(
+                    title: 'IHG78E4 - 21/04/2021: 12.345Km',
+                    leading: Icon(Icons.car_rental)),
+              ],
+            ),
+            CustomSection(
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 22, bottom: 8),
+                    child: Image.asset(
+                      'assets/images/app/settings.png',
+                      height: 50,
+                      width: 50,
+                      color: Color(0xFF777777),
+                    ),
+                  ),
+                  Text(
+                    'Version: 2.4.0 (287)',
+                    style: TextStyle(color: Color(0xFF777777)),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
       ),
       drawer: AppDrawerWidget(),
